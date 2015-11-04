ae6657ff9414eec0a4d990e1343510e3d51a1bee
Aisha Ragheb <aragheb@four51.com>
2015-11-04 17:53:31 -0600

    getting started page

--- app_config/local.js
+++ app_config/local.js
@@ -1,6 +1,6 @@
 //Local Constants
 angular.module('orderCloud')
-    .constant('authurl', 'http://core.four51.com:11629/OAuth/Token')
-    .constant('apiurl', 'http://core.four51.com:9002')
-    .constant('devcenterClientID', '6d60154e-8a55-4bd2-93aa-494444e69996')
+    .constant('authurl', 'https://testauth.ordercloud.io/oauth/token')
+    .constant('apiurl', 'https://testapi.ordercloud.io')
+    .constant('devcenterClientID', '1aa9ed77-64f0-498d-adfa-8b430d7a7858')
     .constant('environment', 'test');

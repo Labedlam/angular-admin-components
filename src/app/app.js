ccf972755f3f225656ba82faa8f923d5dc8e5378
Kyle Olson <kolson@four51.com>
2015-11-04 14:58:23 -0600

    app.js updates for environments

--- src/app/app.js
+++ src/app/app.js
@@ -24,22 +24,7 @@ angular.module( 'orderCloud', [
 	//Constants needed for the OrderCloud AngularJS SDK
 	.constant('ocscope', 'FullAccess')
 	.constant('appname', 'DevCenter')
-
-	//Test Environment
-	//.constant('authurl', 'https://testauth.ordercloud.io/oauth/token')
-	//.constant('apiurl', 'https://testapi.ordercloud.io')
-	//.constant('devcenterClientID', '1aa9ed77-64f0-498d-adfa-8b430d7a7858') //Test
-
-	.constant('authurl', 'http://core.four51.com:11629/OAuth/Token')
-	.constant('apiurl', 'http://core.four51.com:9002')
-	.constant('devcenterClientID', '6d60154e-8a55-4bd2-93aa-494444e69996') //Local
-
 	.constant('devapiurl', 'https://devcenterapi.herokuapp.com')
-	//.constant('devapiurl', 'https://devcenterapi-test.herokuapp.com')
-	//.constant('devapiurl', 'http://localhost:55555')
-
-	.constant('environment', 'test')
-	/*.constant('environment', 'production')*/
 
 ;
 

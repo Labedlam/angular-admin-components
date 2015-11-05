c65b72143daf8344b754cecf2259647389cffcd5
Steve Davis <sdavis@four51.com>
2015-11-04 21:12:22 -0600

    Better local config settings

--- app_config/local.js
+++ app_config/local.js
@@ -1,6 +1,6 @@
 //Local Constants
 angular.module('orderCloud')
-    .constant('authurl', 'https://testauth.ordercloud.io/oauth/token')
-    .constant('apiurl', 'https://testapi.ordercloud.io')
-    .constant('devcenterClientID', '1aa9ed77-64f0-498d-adfa-8b430d7a7858')
+    .constant('authurl', 'http://core.four51.com:11629/oauth/token')
+    .constant('apiurl', 'http://core.four51.com:9002')
+    .constant('devcenterClientID', '6d60154e-8a55-4bd2-93aa-494444e69996') //Local
     .constant('environment', 'test');

762a524e7b1a3b8614652970df11552699a447c1
Kyle Olson <kolson@four51.com>
2015-11-08 19:38:12 -0600

    Disqus updates

--- app_config/local.js
+++ app_config/local.js
@@ -9,6 +9,7 @@ angular.module('orderCloud')
 
  angular.module('orderCloud')
  .constant('authurl', 'http://core.four51.com:11629/oauth/token')
- .constant('apiurl', 'http://core.four51.com:9002')
- .constant('devcenterClientID', '6d60154e-8a55-4bd2-93aa-494444e69996') //Local
- .constant('environment', 'local');
+    .constant('apiurl', 'http://core.four51.com:9002')
+    .constant('integrationurl', 'http://core.four51.com:9004')
+    .constant('devcenterClientID', '6d60154e-8a55-4bd2-93aa-494444e69996') //Local
+    .constant('environment', 'local');
--- app_config/prod.js
+++ app_config/prod.js
@@ -2,5 +2,6 @@
 angular.module('orderCloud')
     .constant('authurl', 'https://auth.ordercloud.io/oauth/token')
     .constant('apiurl', 'https://api.ordercloud.io')
+    .constant('integrationurl', 'https://integrations.ordercloud.io')
     .constant('devcenterClientID', 'b097a9aa-5bc3-4937-b6bb-6ea37e004486')
     .constant('environment', 'production');

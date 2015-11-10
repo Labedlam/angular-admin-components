cfe4d5f2372e2144a4e1f58b84948d8eb4678246
Max Lund <mlund@four51.com>
2015-11-09 20:30:49 -0600

    analytics updates

--- src/app/analytics/DcAnalytics.js
+++ src/app/analytics/DcAnalytics.js
@@ -23,7 +23,30 @@ function AnalyticsConfig( $stateProvider, $httpProvider ) {
 
 
 function DcAnalyticsController() {
- var vm = this;
+    var vm = this;
+    var client = new Keen({
+        projectId: '561c1f5946f9a76e5db2583c',
+        readKey: "348def2aa2aaef3fe5f20b42fdc408ad60fec8814ac60b9fc2ae70af784a030d09053b35fe6cd7498955298d24dc3345d23785ee9dc0a99417f687c09ef117714d8d6be255670443aa6ef17859780a7019dd4d90ee20df2358c797edcc21bafa5884a7b951365a4545db7ac151d664c2"
+    });
+
+    Keen.ready(function() {
+        var savedQueries = client.savedQueries();
+
+        savedQueries.get("daily-registrations",function(err, res) {
+            vm.registrations = res;
+        });
+
+        savedQueries.update("daily-registrations",
+            {refresh_rate: 60},
+            function(err, res) {
+                if (err) {
+                    $exceptionHandler({data: {error: 'Could not update Registration Analytics at this time'}})
+                } else {
+                    vm.registrations = res;
+                }
+            })
+    });
+
 
     vm.test = 'hello';
 }

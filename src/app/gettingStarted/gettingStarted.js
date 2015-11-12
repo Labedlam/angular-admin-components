d174b2d03e525f87b313ccec84b353ba6414894b
Connor O'Brien <cobrien@four51.com>
2015-11-12 13:20:55 -0600

    Changes to getting started. I realized I had forgotten to include a section about changing the client id in the seed. Probably need a little more detail when we start having buyer apps and admin apps

--- src/app/gettingStarted/gettingStarted.js
+++ src/app/gettingStarted/gettingStarted.js
@@ -20,4 +20,10 @@ function GettingStartedConfig( $stateProvider ) {
 
 function GettingStartedController( ) {
     var vm = this;
+    vm.setMaxLines = function(editor) {
+        editor.setOptions({
+            maxLines:1000
+        });
+    };
+    vm.configuration_sample = "//Constants needed for the OrderCloud AngularJS SDK\n.constant('ocscope', 'FullAccess')\n.constant('appname', 'OrderCloud AngularJS Seed')\n\n//Client ID for a Registered Distributor or Buyer Company\n.constant('clientid', '0e0450e6-27a0-4093-a6b3-d7cd9ebc2b8f')\n\n//Test Environment\n.constant('authurl', 'https://testauth.ordercloud.io/oauth/token')\n.constant('apiurl', 'https://testapi.ordercloud.io')";
 }
